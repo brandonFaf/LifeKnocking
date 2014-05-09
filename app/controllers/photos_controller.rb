1cdbf53b723314eb6287a2d4c5f4ef836bc29360
FafStudios <fafstudios@gmail.com>
2014-05-09 13:15:49 -0400

    added Photo model and bootswatch styles

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,4 +2,29 @@ class PhotosController < ApplicationController
 	def index
 		@photo = Photo.all
 	end
+
+	def new
+		@photo = Photo.new
+	end
+
+	def create
+		@photo = Photo.new(photo_params)
+
+		if @photo.save
+			redirect_to action: 'index', notice: 'photo saved'
+		else
+			render action: 'new', alert: 'something went wrong'
+		end
+	end
+
+	def destroy
+		photo = Photo.find(params[:id])
+		photo.destroy
+		redirect_to action: 'index', notice: 'deleted'
+	end
+
+	def photo_params
+		params.require(:photo).permit(:title, :caption)
+	end
+
 end
